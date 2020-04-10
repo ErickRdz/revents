4414766f7f4da2253f8c793caf3b26058ddd2f9e
erick.rodriguez <erick.rodriguez@famsa.com>
2020-04-09 19:07:39 -0500

    Firebase config deleted

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDnB8Q3HC1ZSrMB-AG0wWe-_UTl0MeLLnU",
-    authDomain: "revents-f6c01.firebaseapp.com",
-    databaseURL: "https://revents-f6c01.firebaseio.com",
-    projectId: "revents-f6c01",
-    storageBucket: "revents-f6c01.appspot.com",
-    messagingSenderId: "589894528332",
-    appId: "1:589894528332:web:76823d4cac21bd90c0b3e9"
+    
 };
 
 firebase.initializeApp(firebaseConfig);

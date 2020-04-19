ee34858dd608aa8a54bdaed2db62aedcdceaac86
erick.rodriguez <erick.rodriguez@famsa.com>
2020-04-18 22:32:16 -0500

    End of the section 16

--- src/app/common/form/DateInput.jsx
+++ src/app/common/form/DateInput.jsx
@@ -3,15 +3,30 @@ import { Form, Label } from 'semantic-ui-react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-const DateInput = ({input, width, placeholder, meta: {touched, error}, ...rest}) => {
+const DateInput = ({
+    input: {
+        value,
+        onChange,
+        onBlur
+    }, 
+    width, 
+    placeholder, 
+    meta: {touched, error}, 
+    ...rest
+}) => {
     return (
         <Form.Field error={touched && !!error}>
             <DatePicker 
                 {...rest}
                 placeholderText={placeholder}
-                selected={input.value ? new Date(input.value) : null}
-                onChange={input.onChange}
-                onBlur={input.onBlur}
+                selected={
+                    value 
+                        ? (Object.prototype.toString.call(value) !== '[object Date]') 
+                            ? value.toDate() 
+                            : new Date(value) 
+                        : null}
+                onChange={onChange}
+                onBlur={onBlur}
                 onChangeRaw={(e) => e.preventDefault()}
             />
             {touched && error && <Label basic color='red'>{error}</Label>}
--- src/app/common/form/SelectInput.jsx
+++ src/app/common/form/SelectInput.jsx
@@ -4,7 +4,12 @@ import { Form, Select, Label } from 'semantic-ui-react';
  const SelectInput = ({input, type, placeholder, multiple, options, meta: {touched, error}}) => {
     return (
         <Form.Field error={touched && !!error}>
-            <Select value={input.value || null} 
+            <Select 
+                value={
+                    multiple
+                    ? input.value || []
+                    : input.value || null
+                } 
                 onChange={(e, data) => input.onChange(data.value)}
                 placeholder={placeholder}
                 options={options}

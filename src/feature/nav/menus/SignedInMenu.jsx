cde4ec4ccdc0a1914a377d37260baf5a70e4a706
erick.rodriguez <erick.rodriguez@famsa.com>
2020-04-25 22:17:08 -0500

    Modify profile page link

--- src/feature/nav/menus/SignedInMenu.jsx
+++ src/feature/nav/menus/SignedInMenu.jsx
@@ -11,7 +11,7 @@ const SignedInMenu = ({signOut, profile, auth}) => {
                   <Dropdown.Item text="Create Event" icon="plus" />
                   <Dropdown.Item text="My Events" icon="calendar" />
                   <Dropdown.Item text="My Network" icon="users" />
-                  <Dropdown.Item as={Link} to={`/profile/${auth.id}`}  text="My Profile" icon="user" />
+                  <Dropdown.Item as={Link} to={`/profile/${auth.uid}`}  text="My Profile" icon="user" />
                   <Dropdown.Item as={Link} to='/settings' text="Settings" icon="settings" />
                   <Dropdown.Item onClick={signOut} text="Sign Out" icon="power" />
                 </Dropdown.Menu>
